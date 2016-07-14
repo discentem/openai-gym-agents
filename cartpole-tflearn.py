3a3ed1555b169e510e5221a6bafeeba8b47d7023
Oleg Medvedev <ole.bjorne@gmail.com>
2016-07-13 22:34:08 -0500

    changing to tflearn and summaries

--- cartpole-tflearn.py
+++ cartpole-tflearn.py
@@ -1,9 +1,9 @@
+from __future__ import print_function
 import gym
-import tensorflow as tf
 import tflearn
+import tensorflow as tf
 import numpy as np
 import shutil
-
 
 class ExperienceQModel(object):
     def __init__(self, env, monitor_file, log_dir, max_memory=10000, discount=.9, n_episodes=100, 
@@ -30,13 +30,14 @@ class ExperienceQModel(object):
         self.n_steps = n_steps # must be equal to episode length
         self.batch_size = batch_size
         self.stop_training = stop_training # stop training after stop_training consecutive wins
-        self.consec_wins = 0 # number of consecutive wins to stop training
-        self.global_step = 0 # global step
+        self.consecutive_wins = 0
 
         # Neural Network Parameters
-        self.x = tf.placeholder(tf.float32, [None, self.n_states])
-        self.y = tf.placeholder(tf.float32, [None, self.n_actions])
         self.n_hidden_1 = self.n_states
+        
+        # Initialize tensor flow parameters
+        self.x = tf.placeholder(tf.float32, [None, self.n_states],name='states')
+        self.y = tf.placeholder(tf.float32, [None, self.n_actions],name='qvals')
         
         # Tensorboard directory
         try:
@@ -47,6 +48,7 @@ class ExperienceQModel(object):
         
         # define graph
         self.tf_build_graph()
+        self.tf_build_summaries()
 
     # process reward
     def exp_process_reward(self,ts,reward,endgame):
@@ -88,68 +90,53 @@ class ExperienceQModel(object):
 
     # construct network
     def tf_network(self):
-        net = tflearn.fully_connected(inputs, n_hidden_1, activation='relu')
-        q_values = tflearn.fully_connected(net, self.n_actions)
+        net = tflearn.fully_connected(self.x, self.n_hidden_1, activation='relu',name='hidden1')
+        qvalues = tflearn.fully_connected(net, self.n_actions,name='out')
         return qvalues
 
-    # aux to attach many summaries
-    def tf_variable_summaries(self,var, name):
-        with tf.name_scope('summaries'):
-            mean = tf.reduce_mean(var)
-            tf.scalar_summary('mean/' + name, mean)
-            with tf.name_scope('stddev'):
-                stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
-                tf.scalar_summary('sttdev/' + name, stddev)
-                tf.scalar_summary('max/' + name, tf.reduce_max(var))
-                tf.scalar_summary('min/' + name, tf.reduce_min(var))
-                tf.histogram_summary(name, var)
-
-    # Set up some episode summary ops to visualize on tensorboard.
-    # source - tflearn example
-    def tf_build_summaries():
-        episode_score = tf.Variable(0.)
-        tf.scalar_summary("Reward", episode_score)
-
-        episode_avg_loss = tf.Variable(0.)
-        tf.scalar_summary("Avg Loss", episode_avg_loss)
-        
-        episode_avg_q_max = tf.Variable(0.)
-        tf.scalar_summary("Qmax Value", episode_avg_q_max)
-
-        logged_epsilon = tf.Variable(0.)
-        tf.scalar_summary("Epsilon", logged_epsilon)
-
-        # Threads shouldn't modify the main graph, so we use placeholders
-        # to assign the value of every summary (instead of using assign method
-        # in every thread, that would keep creating new ops in the graph)
-        summary_vars = [episode_score, episode_avg_q_max, logged_epsilon]
-        summary_placeholders = [tf.placeholder("float")
-                                for i in range(len(summary_vars))]
-        assign_ops = [summary_vars[i].assign(summary_placeholders[i])
-                      for i in range(len(summary_vars))]
-        summary_op = tf.merge_all_summaries()
-
-        return summary_placeholders, assign_ops, summary_op
-
-    # Construct model
+    # Construct graph
     def tf_build_graph(self):
         
         # Init session
         self.session = tf.Session()
-        self.network_params = tf.trainable_variables()
 
+        # Model scope
         self.predictor = self.tf_network()
-        self.loss = tf.mean_square(self.predictor,self.y)
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
-        self.grad_update = self.optimizer.minimize(cost, var_list=self.network_params)
+        self.loss = tf.reduce_sum(tf.pow(self.predictor-self.y, 2)/(2*self.batch_size))
+        self.train_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
 
+    # Set up some episode summary ops to visualize on tensorboard.
+    # source - tflearn example
+    def tf_build_summaries(self):
+        episode_score = tf.Variable(0.)
+        tf.scalar_summary("Episode Score", episode_score)
+    
+        episode_avg_loss = tf.Variable(0.)
+        tf.scalar_summary("Episode Avg Loss", episode_avg_loss)
+
+        episode_avg_qmax = tf.Variable(0.)
+        tf.scalar_summary("Episode Avg Qmax", episode_avg_qmax)
+    
+        episode_eps = tf.Variable(0.)
+        tf.scalar_summary("Episode Epsilon", episode_eps)
+    
+        # Threads shouldn't modify the main graph, so we use placeholders
+        # to assign the value of every summary (instead of using assign method
+        # in every thread, that would keep creating new ops in the graph)
+        summary_vars = [episode_score, episode_avg_loss, episode_avg_qmax, episode_eps]
+        self.summary_placeholders = [tf.placeholder("float")
+            for i in range(len(summary_vars))]
+    
+        self.assign_ops = [summary_vars[i].assign(self.summary_placeholders[i])
+            for i in range(len(summary_vars))]
+        
+        self.summary_op = tf.merge_all_summaries()
 
     # Train loop
     def tf_train_model(self):
         # Initializing the session
-        self.session.run(tf.initialize_all_variables())
-        self.summary_op = self.tf_episode_summaries()[-1]
         self.summary_writer = tf.train.SummaryWriter(self.log_dir + '/train', graph=tf.get_default_graph())
+        self.session.run(tf.initialize_all_variables())
 
         # start open ai monitor
         if self.monitor_file:
@@ -175,7 +162,7 @@ class ExperienceQModel(object):
                 state_t1 = np.array(state_tp1)
         
                 # epsilon-greedy exploration
-                if self.consec_wins < self.stop_training and np.random.rand() <= self.eps(epoch):
+                if self.consecutive_wins < self.stop_training and np.random.rand() <= self.eps(epoch):
                     action = self.env.action_space.sample()
                 else:
                     feed_dict = {self.x: state_t1.reshape(1,-1)}
@@ -191,39 +178,38 @@ class ExperienceQModel(object):
 
                 #store experience
                 states['action'] = action
-                states['reward'] = float(reward)
+                states['reward'] = float(reward)/10
                 states['endgame'] = endgame
                 states['state_t'] = np.array(state_t1)
                 states['state_tp1'] = np.array(state_tp1)
                 self.exp_save_to_memory(states)
 
                 # Training loop
-                if self.consec_wins < self.stop_training:
+                if self.consecutive_wins < self.stop_training:
                     # get experience replay
                     x_batch, y_batch = self.exp_get_batch()
                     # create feed dictionary
                     feed_dict = {self.x: x_batch, self.y: y_batch}
                     # training
-                    _, loss, summary = self.session.run([self.grad_update, self.loss, self.merged_summary_op],
+                    _, loss = self.session.run([self.train_op, self.loss],
                         feed_dict=feed_dict)
-                    # add summary to the summary_writer
-                    self.global_step += x_batch.shape[0]
-                    self.summary_writer.add_summary(summary,self.global_step)
-                    # avg loss
                     
+                    # avg loss
                     episode_stats['avg_loss'] += loss
+
 
                 # Check if lost or not
                 if endgame == True:
-                    self.consec_wins = 0
-                    print("{:4d}: score {:3d}, cost {:8.4f}, qval {:8.4f}, eps {:8.4f}".
+                    print ("{:4d}: score {:3d}, cost {:8.4f}, qval {:8.4f}, eps {:8.4f}".
                             format(epoch+1,\
                                 episode_stats['score'],
-                                episode_stats['avg_loss']/t
-                                episode_stats['ave_max_q']/t,
-                                episode_stats['epsilon'])
-                    for i in range(len(episode_stats)):
-                        session.run(assign_ops[i],{summary_placeholders[i]: float(episode_stats[i])})
+                                episode_stats['avg_loss']/t,
+                                episode_stats['avg_max_q']/t,
+                                episode_stats['epsilon']))
+                    for i in episode_stats:
+                        self.session.run(self.assign_ops[i],\
+                            {self.summary_placeholders[i]: float(episode_stats[i])})
+
                     break
                 else:
                     episode_stats['score'] += 1
@@ -244,13 +230,13 @@ if __name__ == "__main__":
 
     model = ExperienceQModel(
         env='CartPole-v0',\
-        monitor_file = 'results/cartpole',\
+        monitor_file = None,\
         log_dir = '/tmp/tf/cartpole-256_1e-3_norm',\
         max_memory=40000,\
         discount=.90,\
         n_episodes=400,\
         n_steps=200,\
-        batch_size=256,\
+        batch_size=10,\
         learning_rate = 1.e-3,\
         dropout = 1.0,\
         exploration = lambda x: 0.1 if x<50 else 0,\
@@ -260,5 +246,5 @@ if __name__ == "__main__":
     model.tf_train_model()
 
     # model.submit_result(
-        # algo_id='alg_BjyWQTp1TCq9zSVyEtXTA',\
-        # api_key='sk_7355XBGvTiqI1GxxakqSYw')
+        # algo_id='',\
+        # api_key='')
